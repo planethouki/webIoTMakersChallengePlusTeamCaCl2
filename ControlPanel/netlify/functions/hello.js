bcaf6d886bbe6ba2ac60b206eb35df97a5753614
planethouki <26595148+planethouki@users.noreply.github.com>
2024-02-22 11:20:12 +0900

    test beebotte

--- ControlPanel/netlify/functions/hello.js
+++ ControlPanel/netlify/functions/hello.js
@@ -1,8 +1,17 @@
 const uuid = require('uuid');
+const bbt = require('beebotte');
 
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ id: uuid.v4() })
-  });
+  const bclient = new bbt.Connector({token: process.env.BEEBOTTE_TOKEN});
+  bclient.write(
+    {channel: 'test', resource: 'res', data: uuid.v4()},
+    function(err, res) {
+      if (err) {
+        return callback(err)
+      }
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      });
+    });
 }
