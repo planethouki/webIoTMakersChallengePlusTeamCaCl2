c983bdd8619d7c189c8eea5262c1d23692279624
planethouki <26595148+planethouki@users.noreply.github.com>
2024-02-22 11:28:12 +0900

    beebotte

--- ControlPanel/netlify/functions/hello.js
+++ ControlPanel/netlify/functions/hello.js
@@ -4,7 +4,7 @@ const bbt = require('beebotte');
 exports.handler = function(event, context, callback) {
   const bclient = new bbt.Connector({token: process.env.BEEBOTTE_TOKEN});
   bclient.write(
-    {channel: 'test', resource: 'res', data: uuid.v4()},
+    {channel: 'cacl2', resource: 'res1', data: uuid.v4()},
     function(err, res) {
       if (err) {
         return callback(err)
