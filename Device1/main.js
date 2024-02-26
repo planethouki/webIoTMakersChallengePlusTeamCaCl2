62abfb87b0172cc7173d1bff8d825f13be0d5c26
planethouki <26595148+planethouki@users.noreply.github.com>
2024-02-26 21:37:51 +0900

    main

--- Device1/main.js
+++ Device1/main.js
@@ -1,5 +1,31 @@
 import sleep from './sleep.js'
 import { getOnce } from "./distance-sensor.js";
+import { loopBlink, fastBlinkOnce } from "./led-indicator.js";
+import mqtt from 'mqtt'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// https://beebotte.com/docs/mqtt
+const client =  mqtt.connect('mqtt://mqtt.beebotte.com',
+  {username: `${process.env.BEEBOTTE_TOKEN}`, password: ''}
+);
+
+client.on('message', async function (topic, message) {
+  console.log(topic + '   ' + message);
+  await Promise.all([
+    fastBlinkOnce(),
+    getOnce()
+  ])
+});
+
+client.subscribe('cacl2/measure');
+
+// setInterval(function() {
+//   client.publish('cacl2/res1', 'Hello World');
+// }, 1000 /* 1 second */);
+
+loopBlink()
 
 while (true) {
   try {
