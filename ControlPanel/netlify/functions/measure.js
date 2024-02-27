8970b4b1895317379a6ecc0452d2713f5178fc18
planethouki <26595148+planethouki@users.noreply.github.com>
2024-02-27 20:27:11 +0900

    measure get

--- ControlPanel/netlify/functions/measure.js
+++ ControlPanel/netlify/functions/measure.js
@@ -3,9 +3,13 @@ const bbt = require('beebotte');
 
 exports.handler = function(event, context, callback) {
   if (event.httpMethod === 'PUT') {
-    return put(event, context, callback);
+    return put(event, context)
+      .then((res) => callback(null, res))
+      .catch((err) => callback(err));
   } else if (event.httpMethod === 'GET') {
-    return get(event, context, callback);
+    return get(event, context)
+      .then((res) => callback(null, res))
+      .catch((err) => callback(err));
   }
 
   return callback(null, {
@@ -13,31 +17,35 @@ exports.handler = function(event, context, callback) {
   });
 }
 
-async function get(event, context, callback) {
+async function get(event, context) {
   const bclient = new bbt.Connector({token: process.env.BEEBOTTE_TOKEN});
-  bclient.read(
-    {channel: 'cacl2', resource: 'measure_result', limit: 5},
-    function(err, res) {
-      if (err) {
-        return callback(err)
-      }
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(res)
+  return new Promise((resolve, reject) => {
+    bclient.read(
+      {channel: 'cacl2', resource: 'measure_result', limit: 5},
+      function(err, res) {
+        if (err) {
+          return reject(err)
+        }
+        resolve({
+          statusCode: 200,
+          body: JSON.stringify(res)
+        })
       });
-    });
+  })
 }
 
-async function put(event, context, callback) {
+async function put(event, context) {
   const bclient = new bbt.Connector({token: process.env.BEEBOTTE_TOKEN});
-  bclient.publish(
-    {channel: 'cacl2', resource: 'measure_trigger', data: 'trigger'},
-    function(err, res) {
-      if (err) {
-        return callback(err)
-      }
-      callback(null, {
-        statusCode: 200
+  return new Promise((resolve, reject) => {
+    bclient.publish(
+      {channel: 'cacl2', resource: 'measure_trigger', data: 'trigger'},
+      function(err, res) {
+        if (err) {
+          return reject(err)
+        }
+        resolve({
+          statusCode: 200
+        })
       });
-    });
+  })
 }
