1a7fcf683be582ea9892d4082e6d09d2f5d031ce
planethouki <26595148+planethouki@users.noreply.github.com>
2024-02-27 18:17:51 +0900

    measure trigger

--- ControlPanel/netlify/functions/measure.js
+++ ControlPanel/netlify/functions/measure.js
@@ -4,7 +4,7 @@ const bbt = require('beebotte');
 exports.handler = function(event, context, callback) {
   const bclient = new bbt.Connector({token: process.env.BEEBOTTE_TOKEN});
   bclient.publish(
-    {channel: 'cacl2', resource: 'measure', data: uuid.v4()},
+    {channel: 'cacl2', resource: 'measure', data: 'trigger'},
     function(err, res) {
       if (err) {
         return callback(err)
