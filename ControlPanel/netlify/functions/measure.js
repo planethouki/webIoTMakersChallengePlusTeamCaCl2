1027f49a36f0fb13b9593bbdd232fad3d0c8ba1c
planethouki <26595148+planethouki@users.noreply.github.com>
2024-02-27 20:04:27 +0900

    measure trigger

--- ControlPanel/netlify/functions/measure.js
+++ ControlPanel/netlify/functions/measure.js
@@ -2,16 +2,20 @@ const uuid = require('uuid');
 const bbt = require('beebotte');
 
 exports.handler = function(event, context, callback) {
+  if (event.httpMethod !== 'PUT') {
+    return callback(null, {
+      statusCode: 405
+    });
+  }
   const bclient = new bbt.Connector({token: process.env.BEEBOTTE_TOKEN});
   bclient.publish(
-    {channel: 'cacl2', resource: 'measure', data: 'trigger'},
+    {channel: 'cacl2', resource: 'measure_trigger', data: 'trigger'},
     function(err, res) {
       if (err) {
         return callback(err)
       }
       callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(res)
+        statusCode: 200
       });
     });
 }
